11d1dfc4dd0d31e72311f6731a482e64255c1cc1
Yann <yann.perennes@epitech.eu>
2023-03-17 09:39:17 +0100

    feat: open file

--- my_file_explorer.c
+++ my_file_explorer.c
@@ -11,7 +11,7 @@ void my_file_explorer(framebuffer_t *buff, sfEvent *event)
 {
     switch (buff->option) {
     case 1: my_new_file(buff, event); break;
-    case 2: my_open_file(buff, event); break;
+    case 2: my_new_file(buff, event); break;
     case 3: my_save_buffer(buff); break;
     }
     return;
